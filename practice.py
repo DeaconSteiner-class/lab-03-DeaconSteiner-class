cf5eb317e1c661526e0a7f27c66f73b57fab0277
DeaconSteiner <deaconsteiner.it@gmail.com>
2024-09-24 18:24:59 -0600

    Completed the TA's guide to lists

--- practice.py
+++ practice.py
@@ -4,3 +4,10 @@ print(table_names)
 table_names.pop(0)
 print(table_names)
 
+table_names.append("Ben")
+print(table_names)
+
+# Use a loop to print through "{name} is at my table"
+
+for name in table_names:
+    print(f"{name} is at my table")
